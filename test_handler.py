df3c3a34ececc0e1630667f1a98830f6c3de4685
shekar.ym <shekar.ym@gmail.com>
2020-12-27 22:19:38 +1100

    first commit

---
+++ test_handler.py
@@ -0,0 +1,56 @@
+import boto3
+import pytest
+import os
+import json
+from contextlib import contextmanager
+from moto import mock_dynamodb2
+from visitors_counter import app
+from unittest import mock
+
+TABLE = "visitors"
+
+
+@contextmanager
+def do_test_setup():
+    with mock_dynamodb2():
+        set_up_dynamodb()
+        yield
+
+
+def set_up_dynamodb():
+    client = boto3.client('dynamodb', region_name='us-east-1')
+    client.create_table(
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'id',
+                'AttributeType': 'S'
+            },
+        ],
+        KeySchema=[
+            {
+                'AttributeName': 'id',
+                'KeyType': 'HASH'
+            }
+        ],
+        TableName=TABLE,
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 1,
+            'WriteCapacityUnits': 1
+        }
+    )
+
+
+@mock.patch.dict(os.environ, {"databaseName": TABLE})
+def test_handler():
+    with do_test_setup():
+        # Run call with an event describing the file:
+        response = app.lambda_handler(None, None)
+
+        # Check that it exists in `processed/`
+        assert response['statusCode'] == 200
+        assert response['body'] == json.dumps({"visitorCount": 1})
+
+        response = app.lambda_handler(None, None)
+
+        assert response['statusCode'] == 200
+        assert response['body'] == json.dumps({"visitorCount": 2})
